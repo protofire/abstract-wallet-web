c0c8c30cea0b71b5c9b2aa03aa30b36e67f42614
msvstj <savastijonok@gmail.com>
2025-05-22 18:17:15 +0300

    feat: new gateway link

--- apps/web/src/config/constants.ts
+++ apps/web/src/config/constants.ts
@@ -3,10 +3,8 @@ import chains from './chains'
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
-export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.abstract-safe.protofire.io'
-export const GATEWAY_URL_STAGING =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.abstract-safe.protofire.io'
+export const GATEWAY_URL_PRODUCTION = process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.safe.abs.xyz'
+export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.safe.abs.xyz'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
