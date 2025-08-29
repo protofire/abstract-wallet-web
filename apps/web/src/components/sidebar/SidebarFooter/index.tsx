1791a5b21e718ce85523be8bbf5fa5269380c07a
den <dzianis.smalonski@protofire.io>
2025-08-29 18:13:06 +0200

    fix: center align caption text in SidebarFooter for improved layout

--- apps/web/src/components/sidebar/SidebarFooter/index.tsx
+++ apps/web/src/components/sidebar/SidebarFooter/index.tsx
@@ -99,7 +99,7 @@ const SidebarFooter = (): ReactElement => {
       </ListItem>
       <ListItem>
         <SidebarListItemText>
-          <Typography variant="caption">
+          <Typography variant="caption" sx={{ mx: 'auto', textAlign: 'center' }}>
             Supported by{' '}
             <SvgIcon
               component={ProtofireLogo}
